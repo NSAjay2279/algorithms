a23e6c8931af5db391b7cbc124731cbf20c1f691
NSAjay2279 <144517147+NSAjay2279@users.noreply.github.com>
2024-12-03 10:46:45 +0000

    Tue, Dec 3, 2024, 4:16 PM +05:30

---
+++ slist.py
@@ -0,0 +1,11 @@
+class SinglyNode:
+
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+
+    def __str__(self):
+        return str(self.val)
+
+Head = SinglyNode(1)
+A = SinglyNode(3)
