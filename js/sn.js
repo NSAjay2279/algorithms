f5c919ba37d2ca783d4cd2e716d9943a104cbbfd
NSAjay2279 <144517147+NSAjay2279@users.noreply.github.com>
2024-12-03 17:41:44 +0000

    Tue, Dec 3, 2024, 11:11 PM +05:30

---
+++ js/sn.js
@@ -0,0 +1,10 @@
+class SinglyNode {
+    constructor(val, next = null) {
+        this.val = val;
+        this.next = next;
+    }
+
+    to String() {
+        return this.val.toString();
+    }
+}
