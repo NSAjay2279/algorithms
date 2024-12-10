e3dc5b63fd7890a6bc3d4374db5aee4c910e4e67
NSAjay2279 <144517147+NSAjay2279@users.noreply.github.com>
2024-12-10 17:16:19 +0000

    Tue, Dec 10, 2024, 10:46 PM +05:30

--- python/snowflakes.py
+++ python/snowflakes.py
@@ -1,2 +1,16 @@
 def identify_identical(values[]: list, n: int):
-    
+    i = 0, j = i + 1;
+    for i in range(n):
+        for j in range(n):
+            if values[i] == values[j]:
+                print("Twin integers found.\");
+                return;
+    print("No two integers are alike.\n")
+
+def main():
+    a = [1, 2, 3, 1, 5]
+    identify_identical(a, 5)
+
+
+if __name__ = "main":
+    main()
