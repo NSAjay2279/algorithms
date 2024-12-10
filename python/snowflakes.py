1a70267cac1d322ba8bbdfbe4270123cf763678e
NSAjay2279 <144517147+NSAjay2279@users.noreply.github.com>
2024-12-10 18:37:59 +0000

    Wed, Dec 11, 2024, 12:07 AM +05:30

--- python/snowflakes.py
+++ python/snowflakes.py
@@ -28,7 +28,7 @@ def are_identical(snow1: list[int], snow2: list):
             return 1
         return 0
 
-def identify identical(snowflakes[][6]: list[list[int]], n: int) -> None:
+def identify_identical(snowflakes[][6]: list[list[int]], n: int) -> None:
     i = 0
     j = i + 1
     for i in range(n):
