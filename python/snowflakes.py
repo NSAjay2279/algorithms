f3f4c307c14896453055f35fac03bb57f8f80365
NSAjay2279 <144517147+NSAjay2279@users.noreply.github.com>
2024-12-11 06:04:21 +0000

    Wed, Dec 11, 2024, 11:34 AM +05:30

--- python/snowflakes.py
+++ python/snowflakes.py
@@ -1,32 +1,49 @@
-MAX_LINES = 100
+SIZE = 100000
+
+def identical_right(snow1: list[int], snow2: list[int], start: int) -> bool:
+    offset = 0
+    for offset in range(6):
+        if snow1[offset] != snow2[(start + offset) % 6]:
+            return False
+    return True
+
+def identical_left(snow1: list[int], snow2: list[int], start: int) -> bool:
+    offset = 0
+    snow2_index = 0
+    for offset in range(6):
+        snow2_index = start - offset
+        if snow2_index <= -1:
+            snow2_index = snow2_index % 6
+        if snow1[offset] != snow2[snow2_index]:
+            return False
+    return True
+
+def are_identical(snow1: list[int], snow2: list[int]) -> bool:
+    start = 0
+    for start in range(6):
+        if identical_right(snow1, snow2, start):
+            return True
+        if identical_left(snow1, snow2, start):
+            return True
+    return False
 
 
-def shortest_line_index(lines, n):
-    j = 1
-    shortest = 0
-    for j in range(n):
-        if lines[j] < lines[shortest]:
-            shortest = j
-    return shortest
-
-
-def solve(lines, n, m):
+def identify_identical(snowflakes: list[list[int]], n: int) -> None:
     i = 0
-    shortest = 0
-    for i in range(m):
-        shortest = shortest_line_index(lines, n)
-        print(lines[shortest])
-        lines[shortest] += 1
-
+    j = 0
+    for i in range(n):
+        for j in range(i+1, n):
+            if are_identical(snowflakes[i], snowflakes[j]):
+                print("Twin snowflakes found.\n")
+                return
+    print("No two snowflakes are alike.\n");
 
 def main():
-    lines = [0 for _ in range(MAX_LINES)]
+    snowflakes = [[0] * 6 for _ in range(SIZE)]
     n = 0
-    m = 0
-    n, m = list(map(int, input().split()))
-    lines = list(map(int, input().split()))
-    solve(lines, n, m)
-
+    n = int(input())
+    snowflakes = [list(map(int, input().split())) for _ in range(n)]
+    identify_identical(snowflakes, n)
 
 if __name__ == "__main__":
     main()
