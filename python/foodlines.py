4c6bfc635977fa1ff2fff8d2172c34af9bade436
NSAjay2279 <144517147+NSAjay2279@users.noreply.github.com>
2024-12-11 05:35:12 +0000

    Wed, Dec 11, 2024, 11:05 AM +05:30

--- python/foodlines.py
+++ python/foodlines.py
@@ -1,27 +1,28 @@
-MAX_LINES = 100
+MAX_LINES: int = 100
 
-def shortest_line_index(lines, n):
-    j = 1
-    shortest = 0
+
+def shortest_line_index(lines: List[int], n: int) -> int:
+    j: int = 1
+    shortest: int = 0
     for j in range(n):
         if lines[j] < lines[shortest]:
             shortest = j
     return shortest
 
 
-def solve(lines, n, m):
-    i = 0
-    shortest = 0
+def solve(lines: List[int], n: int, m: int) -> None:
+    i: int = 0
+    shortest: int = 0
     for i in range(m):
         shortest = shortest_line_index(lines, n)
         print(lines[shortest])
         lines[shortest] += 1
 
 
-def main():
-    lines = [0 for _ in range(MAX_LINES)]
-    n = 0
-    m = 0
+def main() -> None:
+    lines: List[int] = [0 for _ in range(MAX_LINES)]
+    n: int = 0
+    m: int = 0
     n, m = list(map(int, input().split()))
     lines = list(map(int, input().split()))
     solve(lines, n, m)
@@ -29,6 +30,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-
